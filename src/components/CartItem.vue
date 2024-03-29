c61229922bffebe7173d1c4107d1cb3399c03e63
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-03-29 16:05:26 +0500

    Displayed cart in drawer section

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,10 +1,19 @@
+<script setup>
+defineProps({
+  id: Number,
+  title: String,
+  price: Number,
+  imageUrl: String
+})
+</script>
+
 <template>
   <div class="flex items-center gap-5 border border-slate-100 rounded-xl p-4">
-    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Item 1" />
+    <img class="w-16 h-16" :src="imageUrl" :alt="title" />
     <div class="flex flex-col">
-      <p>Мужские Кроссовки Nike Air Max 270</p>
+      <p>{{ title }}</p>
       <div class="flex justify-between">
-        <b>12 999 руб.</b>
+        <b>{{ price }} руб.</b>
         <img class="opacity-50 transition hover:opacity-100" src="/close.svg" alt="Close" />
       </div>
     </div>
