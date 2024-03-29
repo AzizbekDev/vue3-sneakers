5c4a28bb874ca490501a3c87545be0217df1c236
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-03-29 16:23:15 +0500

    Refactor CartItem to use flex-1 for price element

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -10,10 +10,10 @@ defineProps({
 <template>
   <div class="flex items-center gap-5 border border-slate-100 rounded-xl p-4">
     <img class="w-16 h-16" :src="imageUrl" :alt="title" />
-    <div class="flex flex-col">
+    <div class="flex flex-col flex-1">
       <p>{{ title }}</p>
       <div class="flex justify-between">
-        <b>{{ price }} руб.</b>
+        <b class="flex-1">{{ price }} руб.</b>
         <img class="opacity-50 transition hover:opacity-100" src="/close.svg" alt="Close" />
       </div>
     </div>
