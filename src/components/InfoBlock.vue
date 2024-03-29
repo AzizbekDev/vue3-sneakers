42194bb31605aff302abeda1f3e212c3e2445465
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-03-30 03:17:52 +0500

    Add InfoBlock component

---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    imageUrl: String,
+});
+</script>
+
+<template>
+    <div class="flex flex-col items-center text-center w-72 mx-auto">
+        <img height="80" width="80" :src="imageUrl" alt="{{ title }}">
+        <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
+        <p class="mt-2 text-gray-400">{{ description }}</p>
+    </div>
+</template>
