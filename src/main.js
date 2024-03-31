084a8db8020a621322cf728239f031232371c830
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-04-01 03:05:06 +0500

    Add Vue Router for navigation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
+import HomeView from './pages/Home.vue'
+import FavoritesView from './pages/Favorites.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: HomeView },
+    { path: '/favorites', name: "Favorites", component: FavoritesView },
+  ]
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  })
 
 const app = createApp(App)
+app.use(router)
 app.use(autoAnimatePlugin)
 app.mount('#app')
