720ec58726bf557c99415eaaf9c824a6e2421b78
AzizbekDev <azizbek_muzaffarov@mail.ru>
2024-04-02 17:11:04 +0500

    Remove unused code and refactor cart functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, watch, provide, computed } from 'vue'
-import axios from 'axios'
 
 import Header from './components/Header.vue'
 
@@ -9,15 +8,12 @@ import Drawer from './components/Drawer.vue'
 /* Cart */
 const cart = ref([])
 
-const isCreatingOrder = ref(false)
-
 const drawerOpen = ref(false)
 
 const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
 
 const vatPrice = computed(() => Math.round(totalPrice.value * 0.05) / 100)
 
-const cartButtonDisabled = computed(() => !totalPrice.value || isCreatingOrder.value)
 
 const closeDrawer = () => {
   drawerOpen.value = false
@@ -37,22 +33,6 @@ const removeFromCart = (item) => {
   cart.value.splice(cart.value.indexOf(item), 1)
 }
 
-const createOrder = async () => {
-  try {
-    isCreatingOrder.value = true
-    const { data } = await axios.post('https://6e7baa9f3a425a05.mokky.dev/orders', {
-      items: cart.value,
-      totalPrice: totalPrice.value,
-    })
-    cart.value = []
-    return data
-  } catch (err) {
-    console.log(err)
-  } finally {
-    isCreatingOrder.value = false
-  }
-}
-
 watch(cart, () => {
   localStorage.setItem('cart', JSON.stringify(cart.value))
 }, {
@@ -69,8 +49,7 @@ provide('cart', {
 /* End Cart */
 </script>
 <template>
-  <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" :disabled-button="cartButtonDisabled"
-    @create-order="createOrder" />
+  <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" />
   <div class="bg-white w-4/5 m-auto mt-14 rounded-xl shadow-xl">
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
     <div class="p-10">
